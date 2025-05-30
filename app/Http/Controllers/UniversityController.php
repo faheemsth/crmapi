22140ac196e4d3e0097ae367f9c5d85ddf384371
azmatrasheedchohan <azmatchohan01@gmail.com>
2025-05-30 15:46:05 +0500

    number_of_tiles in tool kit

--- app/Http/Controllers/UniversityController.php
+++ app/Http/Controllers/UniversityController.php
@@ -119,6 +119,7 @@ class UniversityController extends Controller
             'status' => 'success',
             'message' => 'University list retrieved successfully.',
             'data' => [
+                'number_of_tiles' => 5,
                 'statuses' => $sortedStatuses,
                 'universities' => $universities->items(),
                 'current_page' => $universities->currentPage(),
