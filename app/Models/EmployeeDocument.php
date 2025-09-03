5d3a4e0f68ca97342af3f473681918bd7b38c3ce
azmatrasheedchohan <azmatchohan01@gmail.com>
2025-09-03 18:56:09 +0500

    employee document uploaded by name

--- app/Models/EmployeeDocument.php
+++ app/Models/EmployeeDocument.php
@@ -9,10 +9,14 @@ class EmployeeDocument extends Model
     protected $fillable = [
         'employee_id','document_id','document_value','created_by'
     ];
-    protected $with = ['user:id,name','documentType:id,name']; // Always eager load this relationship
+    protected $with = ['uploadedby:id,name','user:id,name','documentType:id,name']; // Always eager load this relationship
     public function user()
     {
         return $this->hasOne('App\Models\User', 'id', 'employee_id');
+    } 
+    public function uploadedby()
+    {
+        return $this->hasOne('App\Models\User', 'id', 'created_by');
     }
      public function documentType()
     {
