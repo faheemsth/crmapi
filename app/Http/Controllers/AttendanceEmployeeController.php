b8e82e6c187a1873bf7db6b5bcd31914850e0b09
sherazaleem <107539547+sherazaleem@users.noreply.github.com>
2025-10-06 10:58:50 +0500

    update attandance list

--- app/Http/Controllers/AttendanceEmployeeController.php
+++ app/Http/Controllers/AttendanceEmployeeController.php
@@ -1778,7 +1778,11 @@ public function backuplist(Request $request)
                 'earlyCheckOutReason' => $row->earlyCheckOutReason,
                 'worked_hours' => gmdate('H:i:s', $workedSeconds),
                 'status' => $computedStatus,
-                'late' => $lateSeconds ? gmdate('H:i:s', $lateSeconds) : '00:00:00',
+                'late' => ($clockIn !== '00:00:00' && $row?->shift_start)
+                        ? ($clockIn > $row->shift_start
+                            ? gmdate('H:i:s', Carbon::parse($row->shift_start)->diffInSeconds(Carbon::parse($clockIn)))
+                            : '00:00:00')
+                        : '00:00:00',
                 'early_leaving' => $row->early_leaving ?? '00:00:00',
                 'overtime' => $row->overtime ?? '00:00:00',
             ]);
