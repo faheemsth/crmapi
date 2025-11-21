acb95cf58eb865a0a4b628bcf1efd0dfeee64cb8
azmatrasheedchohan <azmatchohan01@gmail.com>
2025-11-21 19:01:30 +0500

    react toolkit tiles number

--- app/Http/Controllers/UniversityController.php
+++ app/Http/Controllers/UniversityController.php
@@ -131,7 +131,7 @@ class UniversityController extends Controller
             'status' => 'success',
             'message' => 'University list retrieved successfully.',
             'data' => [
-                'number_of_tiles' => 5,
+                'number_of_tiles' => 10,
                 'statuses' => $sortedStatuses,
                 'universities' => $universities->items(),
                 'current_page' => $universities->currentPage(),
