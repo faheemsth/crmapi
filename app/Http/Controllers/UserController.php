bbf570424635fd0d73cd217466c137f96b38cf12
Sheraz Aleem <sherazaleem015@gmail.com>
2025-12-19 12:30:42 +0500

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3864,11 +3864,11 @@ public function getDashboardholiday(Request $request)
         $user->profile_status = $statusText;
         $user->comment = $user->blocked_reason;
         if($statusText === 'Approved'){
-            $role_r = Role::findByName($user->type, 'web');
+            $role_r = Role::findByName($user->type);
             $user->assignRole($role_r);
         }
         if($statusText === 'Rejected'){
-            $role_r = Role::findByName($user->type, 'web');
+            $role_r = Role::findByName($user->type);
             $user->removeRole($role_r);
         }
         // email template
