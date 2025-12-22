e7c1630947d618377d56b656531d32f5314b1940
azmatrasheedchohan <azmatchohan01@gmail.com>
2025-12-22 16:16:16 +0500

    user id by leads

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -186,7 +186,7 @@ class LeadController extends Controller
         } elseif ($userType === 'Branch Manager' && $usr->branch_id) {
             $leadsQuery->where('leads.branch_id', $usr->branch_id);
         } elseif ($userType === 'Agent') {
-            $leadsQuery->where('leads.created_by', $usr->agent_id);
+            $leadsQuery->where('leads.user_id', $usr->agent_id);
         }
 
         // Apply Search Filters
